2b044201b85b201fcedb4675dbe3912bfc3ebdc0
xavier <xavier@futurae.com>
2023-07-12 19:10:22 +0200

    Add img modal

---
+++ website/assets/script.js
@@ -0,0 +1,20 @@
+window.onload = function(){  
+	var modal = document.getElementById("imageModal");
+	var modalImg = document.getElementById("modalImg");
+
+
+	modal.onclick = function() {
+		modal.style.display = "none";
+	}
+
+	$("img").on("click", function(){
+		modal.style.display = "flex";
+		modalImg.src = this.src;
+	})
+
+	$(document).on("keyup", function(event) {
+		if(event.key == "Escape") {
+			modal.style.display = "none";
+		}
+	})
+}
